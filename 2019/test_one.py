e75dbb57f98dc73d478e0f25e231073a06a5c0b8
Derek Ratcliff <derek.ratcliff@gmail.com>
2019-12-05 16:17:39 -0500

    Day one in python

---
+++ 2019/test_one.py
@@ -0,0 +1,34 @@
+def getLines(filename):
+    my_list = []
+
+    with open(filename) as fp:
+        line = fp.readline()
+        while line:
+            my_list.append(int(line))
+            line = fp.readline()
+    return my_list
+
+def calculate(my_list):
+    sum = 0
+    for l in my_list:
+        sum = sum + int(l/3) - 2
+    return sum
+
+def test_calculate():
+    lines = getLines("1.txt")
+    answer = calculate(lines)
+    assert answer == 3406527
+
+def calculate_part_two(my_list):
+    sum = 0
+    for l in my_list:
+        fuel = int(l/3) - 2
+        while fuel >= 0:
+            sum = sum + fuel
+            fuel = int(fuel/3) - 2
+    return sum
+
+def test_calculate_part_two():
+    lines = getLines("1.txt")
+    answer = calculate_part_two(lines)
+    assert answer == 5106932
