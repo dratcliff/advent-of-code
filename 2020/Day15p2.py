c5ad5906ca36d1f373643f9e174b40d6fbe5e57b
Derek Ratcliff <derek.ratcliff@gmail.com>
2020-12-15 17:03:05 -0500

    Make Day15p2 faster

--- 2020/Day15p2.py
+++ 2020/Day15p2.py
@@ -1,50 +1,57 @@
-import utils
+from utils import timed
 
-entries = utils.file_to_string_list("Day15.txt")
-test_entries = utils.file_to_string_list("Day15sample.txt")
+class Sequence:
+    def __init__(self, initial, max_size):
+        self.initial = initial
+        self.length = len(initial)
+        lookup = [-1 for i in range(0, max_size)]
+        for i in range(0, self.length-1):
+            lookup[initial[i]] = i + 1
+        self.lookup = lookup
+        self.last = self.initial[self.length-1]
 
-last2 = {}
-def next(so_far, last, length):
-    """So, after the starting numbers, each turn results in that player speaking aloud either 0 (if the last number is new) or an age (if the last number is a repeat)."""
-    if len(so_far[last]) == 1:
-        so_far[0].append(length+1)
-        last = 0
-    else:
-        last2 = so_far[last][-2:]
-        diff = last2[1]-last2[0]
-        if diff in so_far:
-            so_far[diff].append(length+1)
-        else:
-            so_far[diff] = [length+1]
-        last = diff
-    return (so_far, last, length+1)
-    
+    def generate_until(self, limit):
+        length = self.length
+        lookup = self.lookup
+        last = self.last
+        while length < limit:
+            """So, after the starting numbers, each turn results in that player speaking aloud either 0 (if the last number is new) or an age (if the last number is a repeat)."""
+            if lookup[last] != -1:
+                diff = length - lookup[last]
+                n = diff
+            else:
+                n = 0
+            lookup[last] = length
+            last = n
+            length += 1
+        self.lookup = lookup
+        self.last = last
+        self.length = length
 
+        return last
+
+@timed
 def test_day_fifteen():
-    #2,0,6,12,1,3
-    n = {
-        2: [1],
-        0: [2],
-        6: [3],
-        12: [4],
-        1: [5],
-        3: [6]
-    }
-    so_far, last, length = next(n, 3, 6)
-    for i in range(0, 30000000):
-        if i % 30000000 == 0:
-            print(i)
-        so_far, last, length = next(so_far, last, length)
-    
-    for k, v in so_far.items():
-        if 30000000 in v:
-            print(k, v)
+    seq = [0, 3, 6]
+    g = Sequence(seq, 2020)
+    n = g.generate_until(2020)
+    assert n == 436
 
+@timed
 def day_fifteen():
-    pass
-    # print(entries)
+    seq = [2,0,6,12,1,3]
+    g = Sequence(seq, 2020)
+    n = g.generate_until(2020)
+    assert n == 1428
 
+@timed
+def day_fifteen_part_two():
+    seq = [2,0,6,12,1,3]
+    g = Sequence(seq, 30000000)
+    n = g.generate_until(30000000)
+    assert n == 3718541
 
 if __name__=="__main__":
     test_day_fifteen()
     day_fifteen()
+    day_fifteen_part_two()
