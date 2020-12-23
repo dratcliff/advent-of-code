da31c746a49c9639b5cbe557bc853cce934859bd
Derek Ratcliff <derek.ratcliff@gmail.com>
2020-12-23 14:45:10 -0500

    Make part two a little faster

--- 2020/Day23p2.py
+++ 2020/Day23p2.py
@@ -8,38 +8,33 @@ class Cup:
 
 def init(starting_string, size):
 
-    m = {}
+    m = [Cup(0, None) for x in range(size+1)]
     for i in range(0, len(starting_string)):
         v = int(starting_string[i])
-        m[v] = Cup(v, None)
+        m[v].label = v
     
     for i in range(0, len(starting_string)-1):
         v = int(starting_string[i])
         w = int(starting_string[i+1])
         m[v].clockwise_cup = m[w]
     
-    last = int(starting_string[-1])
+    last_int = int(starting_string[-1])
     first = int(starting_string[0])
-    biggest = max(m.keys())    
+    biggest = max([int(x) for x in starting_string] )
 
-    # print(m, biggest)
-
-    for i in range(biggest+1, size+1):
-        m[i] = Cup(i, None)
-
-    m[last].clockwise_cup = m[biggest+1]
-    # print("mlast", m[last].clockwise_cup.label)
-
+    last = Cup(biggest+1, None)
+    m[last_int].clockwise_cup = last
+    m[biggest+1] = last
     for i in range(biggest+1, size):
-        m[i].clockwise_cup = m[i+1]
-
+        next_cup = m[i+1]
+        last.clockwise_cup = next_cup
+        next_cup.label = i+1
+        last = next_cup
     m[size].clockwise_cup = m[first]
 
-    m["max"] = max(m.keys())
-    m["cur"] = first
-    return m
+    return (m, size, m[first])
 
-def move(m):
+def move(m, biggest, current):
     """
     The crab picks up the three cups that are immediately clockwise of the current cup. 
     They are removed from the circle; cup spacing is adjusted as necessary to maintain the circle.
@@ -54,37 +49,29 @@ def move(m):
     The crab selects a new current cup: the cup which is immediately clockwise of the current cup.
     """
 
-    # pick up
-    current = m[m["cur"]]
-    # print("current", current.label)
     c1 = current.clockwise_cup
     c2 = c1.clockwise_cup
     c3 = c2.clockwise_cup
-
     current.clockwise_cup = c3.clockwise_cup
-    m["cur"] = c3.clockwise_cup.label
-
-    labels = set([c1.label, c2.label, c3.label])
-    # print("neighbors", labels)
-    # destination
+    result = c3.clockwise_cup
     destination = current.label - 1
-    while destination in labels:
+    while destination == c1.label or destination == c2.label or destination == c3.label:
         destination -= 1
     
     if destination <= 0:
-        destination = m["max"]
-    # print("destination", destination)
-    c3.clockwise_cup = m[destination].clockwise_cup
-    m[destination].clockwise_cup = c1
+        destination = biggest
+    at_destination = m[destination]
+    c3.clockwise_cup = at_destination.clockwise_cup
+    at_destination.clockwise_cup = c1
 
-    return m
+    return result
 
 @utils.timed
 def test_day_twenty_three():
     starting_string = "389125467"
-    m = init(starting_string, 1_000_000)
+    (m, biggest, cur) = init(starting_string, 1_000_000)
     for i in range(0, 10_000_000):
-        m = move(m)
+        cur = move(m, biggest, cur)
 
     one = m[1]
     first = one.clockwise_cup
@@ -95,9 +82,9 @@ def test_day_twenty_three():
 @utils.timed
 def day_twenty_three():
     starting_string = "123487596"
-    m = init(starting_string, 1_000_000)
+    (m, biggest, cur) = init(starting_string, 1_000_000)
     for i in range(0, 10_000_000):
-        m = move(m)
+        cur = move(m, biggest, cur)
 
     one = m[1]
     first = one.clockwise_cup
